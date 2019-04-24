fc9b8b6864af551f13e2586ca83a48c7036b0c95
sacreutz <sacreutz@gmail.com>
2019-04-24 00:08:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(line, person){
   line.push(person)
-  let position = line[person]
-  
+  let position = line.length 
+  return 'Welcome, ' + person + '. You are number ' + position + 'in line.'
   
 }
